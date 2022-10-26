f2ff03c771f968fd223bee486366e0a63366f2cd
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-10-26 13:42:32 +0200

    eslint format

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-const path = require("path");
+/* eslint-disable prettier/prettier */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001,
+  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
