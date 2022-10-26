87b6c201899dc28763fd427e319a369b272b5adb
Mehdy Guettache <mehdy33852@gmail.com>
2022-10-26 13:47:26 +0200

    test

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001,
+  },
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
