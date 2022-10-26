f55c3d42f14a5cb71c4350fa70a2caab2bb3ea64
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-10-26 16:49:46 +0200

    first page component

---
+++ frontend/src/components/Homepage/Homepage.jsx
@@ -0,0 +1,22 @@
+import "./Homepage.css";
+// import Titlesblock from "./components/Titlesblock.jsx"
+
+function Homepage() {
+  return (
+    <div>
+      {/* <Titlesblock /> */}
+      <img
+        className="cocktail__mainpage"
+        src="/src/assets/images/pink_cocktailtrans.png"
+        alt="pink cocktail"
+      />
+      <img
+        className="mintleaves"
+        src="/src/assets/images/Fresh-mint-leaves--on-transparent-background-PNG.png"
+        alt="mintleaves"
+      />
+    </div>
+  );
+}
+
+export default Homepage;
