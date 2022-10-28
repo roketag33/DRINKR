631e359c4b1c8b8f74f8b93a100c9b42b64c4fd1
Mehdy Guettache <mehdy33852@gmail.com>
2022-10-28 09:06:04 +0200

    update majority block

--- frontend/src/components/MajorityBlock/MajorityBlock.jsx
+++ frontend/src/components/MajorityBlock/MajorityBlock.jsx
@@ -1,26 +1,23 @@
 import "./MajorityBlock.css";
 import React from "react";
 
-
-
 function MajorityBlock() {
-  
   return (
-    
-
     <div className="MajorityBlock__container">
       <p className="MajorityBlock__para">Are you 18 years or older ?</p>
 
       <div className="MajorityBlock__buttons">
-        
-        <a  type="button" className="MajorityBlock__button-yes" >
+        <button type="button" className="MajorityBlock__button-yes">
           Yes
-        </a>
-        <a href="https://www.disneyplus.com/fr-fr" type="button" className="MajorityBlock__button-no">
+        </button>
+        <a
+          href="https://www.disneyplus.com/fr-fr"
+          type="button"
+          className="MajorityBlock__button-no"
+        >
           No
         </a>
       </div>
-    
     </div>
   );
 }
