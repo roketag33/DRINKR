179f88727ae012d051f4df29182b3c0ab9afd53c
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-10-28 09:50:35 +0200

    ADD .map formIngredients

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import MajorityBlock from "./components/MajorityBlock/MajorityBlock";
 import "./App.css";
 import Homepage from "./components/Homepage/Homepage";
+import FormIngredients from "./components/FormIngredients/FormIngredients";
 
 function App() {
   return (
     <div className="App">
       <MajorityBlock />
       <Homepage />
+      <FormIngredients />
     </div>
   );
 }
