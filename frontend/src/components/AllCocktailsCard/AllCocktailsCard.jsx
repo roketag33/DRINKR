31c9be830ee74aa9ad433012cd9a9ee88df64b5d
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-11-04 00:07:49 +0100

    completed the allcocktails page with Alex's code and passed some props

--- frontend/src/components/AllCocktailsCard/AllCocktailsCard.jsx
+++ frontend/src/components/AllCocktailsCard/AllCocktailsCard.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import "./AllCocktailsCard.css";
 
-const AllCocktailsCard = () => {
+const AllCocktailsCard = ({ cocktail }) => {
   return (
     <div className="allcocktails__card">
       <div className="allcocktails__frontcard">
-        <img src=" " alt=" " className="allcocktails__Front-img" />
-        <h1 className="allcocktails__title">Title here</h1>
+        <img
+          src={cocktail.strDrinkThumb}
+          alt={cocktail.strDrink}
+          className="allcocktails__img"
+        />
+        <h1 className="allcocktails__title">{cocktail.strDrink}</h1>
       </div>
       <div className="allcocktails__backcard">
         <h5>Ingredients: </h5>
         <ul className="allcocktails__ingredients" />
         <h5> Recipe :</h5>
+        <p>{cocktail.strInstructions}</p>
       </div>
     </div>
   );
