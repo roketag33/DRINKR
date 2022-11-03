40a2b964a99bc37e43819ca5b14824521830b8dc
Olga Yasnopolskaya <olga_yasn@hotmail.com>
2022-11-03 14:52:08 +0100

    added a new page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import CocktailRecipe from "./components/CocktailRecipe/CocktailRecipe";
 import FormIngredients from "./components/FormIngredients/FormIngredients";
 import Homepage from "./components/Homepage/Homepage";
 import ButtonList from "./components/Buttonlist/ButtonList";
+import AllCocktails from "./components/AllCocktails/AllCocktails";
 import "./App.css";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/ButtonList" element={<ButtonList />} />
           <Route path="/FormIngredients" element={<FormIngredients />} />
           <Route path="/CocktailRecipe/:id" element={<CocktailRecipe />} />
+          <Route path="/AllCocktails" element={<AllCocktails />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/components/titleblock/title/Title.jsx
+++ frontend/src/components/titleblock/title/Title.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import "./title.css";
 
 const Title = () => {
-  return <h1 className="main__title-logo">DRINK'R</h1>;
+  return (
+    <NavLink to="/ButtonList">
+      <h1 className="main__title-logo">DRINK'R</h1>
+    </NavLink>
+  );
 };
 
 export default Title;
