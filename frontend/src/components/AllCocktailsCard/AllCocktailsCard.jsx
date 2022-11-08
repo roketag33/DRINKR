12970895ed1267920895198032f45fb3d8465a73
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-11-08 09:32:55 +0100

    Add reverse card Allcocktail

--- frontend/src/components/AllCocktailsCard/AllCocktailsCard.jsx
+++ frontend/src/components/AllCocktailsCard/AllCocktailsCard.jsx
@@ -3,20 +3,22 @@ import "./AllCocktailsCard.css";
 
 const AllCocktailsCard = ({ cocktail }) => {
   return (
-    <div className="allcocktails__card">
-      <div className="allcocktails__frontcard">
-        <img
-          src={cocktail.strDrinkThumb}
-          alt={cocktail.strDrink}
-          className="allcocktails__img"
-        />
-        <h1 className="allcocktails__title">{cocktail.strDrink}</h1>
-      </div>
-      <div className="allcocktails__backcard">
-        <h5>Ingredients: </h5>
-        <ul className="allcocktails__ingredients" />
-        <h5> Recipe :</h5>
-        <p>{cocktail.strInstructions}</p>
+    <div className="container_card">
+      <div className="allcocktails__card">
+        <div className="allcocktails__frontcard">
+          <img
+            src={cocktail.strDrinkThumb}
+            alt={cocktail.strDrink}
+            className="allcocktails__img"
+          />
+          <h1 className="allcocktails__title">{cocktail.strDrink}</h1>
+        </div>
+        <div className="allcocktails__backcard">
+          <h5>Ingredients: </h5>
+          <ul className="allcocktails__ingredients" />
+          <h5> Recipe :</h5>
+          <p>{cocktail.strInstructions}</p>
+        </div>
       </div>
     </div>
   );
--- frontend/src/components/AllCocktailsIngredients/AllCocktailsIngredients.jsx
+++ frontend/src/components/AllCocktailsIngredients/AllCocktailsIngredients.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Title from "../titleblock/title/Title";
 import AllCocktailsCardIngredients from "./AllCocktailsCardIngredients";
+import "../AllCocktails/AllCocktails.css";
 
 const AllCocktailsIngredients = ({ propsFetche }) => {
   return (
@@ -16,7 +17,7 @@ const AllCocktailsIngredients = ({ propsFetche }) => {
       </div>
       <img
         className="green_drinks"
-        src="./src/assets/images/green_cocktail.png"
+        src="/src/assets/images/green_cocktail.png"
         alt="green_cocktail"
       />
     </div>
