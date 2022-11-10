85ed70a4bbf4d70c351afa2a086cacb27a4a039c
ANTHONY LABADIE <anthony.labadie@outlook.fr>
2022-11-10 12:28:41 +0100

    update

--- frontend/src/components/AllCocktailsIngredients/AllCocktailsCardIngredients.jsx
+++ frontend/src/components/AllCocktailsIngredients/AllCocktailsCardIngredients.jsx
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
+import "../AllCocktailsCard/AllCocktailsCard.css";
 
 const AllCocktailsCardIngredients = ({ propsFetche }) => {
   const [cocktail, setCocktail] = useState({});
